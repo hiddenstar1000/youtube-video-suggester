4fa7221bcd7278b3b60c2636f4873cb8f0fefbde
Dixon Gunasekara <dixon@randomsoftware.net>
2024-01-15 20:36:03 +0530

    Add videos

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
 $(document).ready(function () {
-  $("#videoIframe").attr("src", "https://www.youtube.com/embed/ybfyLfI5Ml0");
+  const nextVideo = getNextVideo();
+
+  $("#videoIframe").attr("src", `https://www.youtube.com/embed/${nextVideo}`);
 });
+
+function getNextVideo() {
+  const videoList = [
+    // https://www.youtube.com/@easylanguages
+    // Easy Spanish - Learning Spanish from the Streets
+    "L_-KQ90IiF0", // 1
+    "WlknUGwKHtw", // 2
+  ];
+  const index = Math.floor(Math.random() * videoList.length);
+
+  return videoList[index];
+}
