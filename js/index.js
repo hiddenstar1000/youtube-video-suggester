06554e8b778201a32a97ddeb83aa49ba0b599579
Dixon Gunasekara <dixon@randomsoftware.net>
2024-01-15 22:03:27 +0530

    Add videos

--- js/index.js
+++ js/index.js
@@ -6,10 +6,21 @@ $(document).ready(function () {
 
 function getNextVideo() {
   const videoList = [
+    // https://www.youtube.com/@EnglishSpeeches
+    "fBnAMUkNM2k", // ENGLISH SPEECH | MUNIBA MAZARI - We all are Perfectly Imperfect (English Subtitles)
+    // Learn English with Speeches
     // https://www.youtube.com/@easylanguages
-    // Easy Spanish - Learning Spanish from the Streets
+    // Easy Spanish Podcast - 01
+    // Spanish from Argentina - 02
+    // International episodes - 03
+    // Spanish from Spain - 04
+    // Funny Episodes - 05
+    // Super Easy Spanish - Grammar and Vocab Explained - 06
+    // Spanish from Mexico - 07
+    // Easy Spanish - Learning Spanish from the Streets - 08
     "L_-KQ90IiF0", // 1
     "WlknUGwKHtw", // 2
+    // Super Easy Spanish - Spanish for Beginners - 09
   ];
   const index = Math.floor(Math.random() * videoList.length);
 
