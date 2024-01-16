86ecb2ccaf81644c409fa6efaa07dd7a188b7b29
Dixon Gunasekara <dixon@randomsoftware.net>
2024-01-16 05:44:23 +0530

    Add videos

--- js/index.js
+++ js/index.js
@@ -1,14 +1,22 @@
 $(document).ready(function () {
-  const nextVideo = getNextVideo();
+  loadNextVideo();
 
-  $("#videoIframe").attr("src", `https://www.youtube.com/embed/${nextVideo}`);
+  $("body").keypress(function (e) {
+    const ENTER_KEY = 13;
+
+    if (e.which === ENTER_KEY) loadNextVideo();
+  });
 });
 
-function getNextVideo() {
+function loadNextVideo() {
   const videoList = [
     // https://www.youtube.com/@EnglishSpeeches
     // Learn English with Speeches
     "fBnAMUkNM2k", // ENGLISH SPEECH | MUNIBA MAZARI - We all are Perfectly Imperfect (English Subtitles)
+    "s5BMcaQsjbM", // ENGLISH SPEECH | SAMANTHA RUTH PRABHU: You Can Do It (English Subtitles)
+    "1i9kcBHX2Nw", // ENGLISH SPEECH | STEVE JOBS: Stanford Speech(English Subtitles)
+    "iBBB-vJZB50", // ENGLISH SPEECH | MALALA YOUSAFZAI - Nobel Peace Prize (English Subtitles)
+    "jG-4kBIDAzA", // ENGLISH SPEECH | SELENA GOMEZ: Trust Yourself (English Subtitles)
     // https://www.youtube.com/@easylanguages
     // Easy Spanish Podcast - 01
     // Spanish from Argentina - 02
@@ -20,9 +28,15 @@ function getNextVideo() {
     // Easy Spanish - Learning Spanish from the Streets - 08
     "L_-KQ90IiF0", // 1
     "WlknUGwKHtw", // 2
+    "W0_GwKe1SB8", // 3
+    "m4rPXkaelKw", // 4
+    "0jyMWq5T0eo", // 5
     // Super Easy Spanish - Spanish for Beginners - 09
   ];
   const index = Math.floor(Math.random() * videoList.length);
 
-  return videoList[index];
+  $("#videoIframe").attr(
+    "src",
+    `https://www.youtube.com/embed/${videoList[index]}`
+  );
 }
