9c156673fb151a945f209aa6356bd5da7a3380d0
Dixon Gunasekara <dixon@randomsoftware.net>
2024-01-17 19:56:35 +0530

    Add videos

--- js/index.js
+++ js/index.js
@@ -12,13 +12,16 @@ function loadNextVideo() {
   const videoList = [
     // https://www.youtube.com/@EnglishSpeeches
     // Learn English with Speeches
-    "fBnAMUkNM2k", // ENGLISH SPEECH | MUNIBA MAZARI - We all are Perfectly Imperfect (English Subtitles)
-    "s5BMcaQsjbM", // ENGLISH SPEECH | SAMANTHA RUTH PRABHU: You Can Do It (English Subtitles)
-    "1i9kcBHX2Nw", // ENGLISH SPEECH | STEVE JOBS: Stanford Speech(English Subtitles)
-    "iBBB-vJZB50", // ENGLISH SPEECH | MALALA YOUSAFZAI - Nobel Peace Prize (English Subtitles)
-    "jG-4kBIDAzA", // ENGLISH SPEECH | SELENA GOMEZ: Trust Yourself (English Subtitles)
-    "r01DOmPpxjg", // ENGLISH SPEECH | MUNIBA MAZARI: Motivational Words (English Subtitles)
-    "nIwU-9ZTTJc", // ENGLISH SPEECH | EMMA WATSON: Gender Equality (English Subtitles)
+    "fBnAMUkNM2k", // ENGLISH SPEECH | MUNIBA MAZARI - We all are Perfectly Imperfect (English Subtitles) - 001
+    "s5BMcaQsjbM", // ENGLISH SPEECH | SAMANTHA RUTH PRABHU: You Can Do It (English Subtitles) - 002
+    "1i9kcBHX2Nw", // ENGLISH SPEECH | STEVE JOBS: Stanford Speech(English Subtitles) - 003
+    "iBBB-vJZB50", // ENGLISH SPEECH | MALALA YOUSAFZAI - Nobel Peace Prize (English Subtitles) - 004
+    "jG-4kBIDAzA", // ENGLISH SPEECH | SELENA GOMEZ: Trust Yourself (English Subtitles) - 005
+    "r01DOmPpxjg", // ENGLISH SPEECH | MUNIBA MAZARI: Motivational Words (English Subtitles) - 006
+    "nIwU-9ZTTJc", // ENGLISH SPEECH | EMMA WATSON: Gender Equality (English Subtitles) - 007
+    "aDG1T0kJnd4", // ENGLISH SPEECH | PRIYANKA CHOPRA: Be Fearless (English Subtitles) - 008
+    "_nUDuXRh3zM", // ENGLISH SPEECH | IVANKA TRUMP: Think Big Again (English Subtitles) - 009
+    "_POoaOQ2Xuc", // ENGLISH SPEECH | PRIYANKA CHOPRA: Full Power of Women (English Subtitles) - 010
     // https://www.youtube.com/@easylanguages
     // Easy Spanish Podcast - 01
     // Spanish from Argentina - 02
@@ -28,11 +31,16 @@ function loadNextVideo() {
     // Super Easy Spanish - Grammar and Vocab Explained - 06
     // Spanish from Mexico - 07
     // Easy Spanish - Learning Spanish from the Streets - 08
-    "L_-KQ90IiF0", // 1
-    "WlknUGwKHtw", // 2
-    "W0_GwKe1SB8", // 3
-    "m4rPXkaelKw", // 4
-    "0jyMWq5T0eo", // 5
+    "L_-KQ90IiF0", // Professions | Easy Spanish 1
+    "WlknUGwKHtw", // Plans for today | Easy Spanish 2
+    "W0_GwKe1SB8", // What do you like about Lima? | Easy Spanish 3
+    "m4rPXkaelKw", // Three Wishes | Easy Spanish 4
+    "0jyMWq5T0eo", // What would you never lend to anyone? | Easy Spanish 5
+    "nb5h1svrFro", // The greatest achievement of your life! | Easy Spanish 6
+    "3Ee9cKEPOe4", // What do you believe in? / En qué crees? | Easy Spanish 7
+    "6Pfm-y40NcU", // London | Easy Spanish 8
+    "GBbHQv4FxmQ", // Is global warming real? | Easy Spanish 9
+    "3QKtKxaAZ6o", // In the Mexican Subway | Easy Spanish 10
     // Super Easy Spanish - Spanish for Beginners - 09
   ];
   const index = Math.floor(Math.random() * videoList.length);
