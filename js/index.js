867e8440ec0f721b075d14afc94f19f067187412
Dixon Gunasekara <dixon@randomsoftware.net>
2024-01-19 15:40:44 +0530

    Add videos

--- js/index.js
+++ js/index.js
@@ -27,33 +27,26 @@ function loadNextVideo(languages) {
   const englishVideoList = [
     // https://www.youtube.com/@EnglishSpeeches
     // Learn English with Speeches
-    "fBnAMUkNM2k", // ENGLISH SPEECH | MUNIBA MAZARI - We all are Perfectly Imperfect (English Subtitles) - 001
-    "s5BMcaQsjbM", // ENGLISH SPEECH | SAMANTHA RUTH PRABHU: You Can Do It (English Subtitles) - 002
-    "1i9kcBHX2Nw", // ENGLISH SPEECH | STEVE JOBS: Stanford Speech(English Subtitles) - 003
-    "iBBB-vJZB50", // ENGLISH SPEECH | MALALA YOUSAFZAI - Nobel Peace Prize (English Subtitles) - 004
-    "jG-4kBIDAzA", // ENGLISH SPEECH | SELENA GOMEZ: Trust Yourself (English Subtitles) - 005
-    "r01DOmPpxjg", // ENGLISH SPEECH | MUNIBA MAZARI: Motivational Words (English Subtitles) - 006
-    "nIwU-9ZTTJc", // ENGLISH SPEECH | EMMA WATSON: Gender Equality (English Subtitles) - 007
-    "aDG1T0kJnd4", // ENGLISH SPEECH | PRIYANKA CHOPRA: Be Fearless (English Subtitles) - 008
-    "_nUDuXRh3zM", // ENGLISH SPEECH | IVANKA TRUMP: Think Big Again (English Subtitles) - 009
-    "_POoaOQ2Xuc", // ENGLISH SPEECH | PRIYANKA CHOPRA: Full Power of Women (English Subtitles) - 010
-    "EQYWOQ-12c8", // ENGLISH SPEECH | RASHMIKA MANDANNA: Dream BIG! (English Subtitles) - 011
-    "U8iy5zvXhJc", // ENGLISH SPEECH | SHAKIRA: Education Changes the World (English Subtitles) - 012
-    "2mqQMA7CSLQ", // ENGLISH SPEECH | DONALD TRUMP: Never, Ever Give Up (English Subtitles) - 013
-    "mK53d6aIG48", // ENGLISH SPEECH | R. MADHAVAN: India in 2030 (English Subtitles) - 014
-    "KLe7Rxkrj94", // ENGLISH SPEECH | THE ROCK: Be Yourself (English Subtitles) - 015
+    "fBnAMUkNM2k", // ENGLISH SPEECH | MUNIBA MAZARI - We all are Perfectly Imperfect (English Subtitles) - 1
+    "s5BMcaQsjbM", // ENGLISH SPEECH | SAMANTHA RUTH PRABHU: You Can Do It (English Subtitles) - 2
+    "1i9kcBHX2Nw", // ENGLISH SPEECH | STEVE JOBS: Stanford Speech(English Subtitles) - 3
+    "iBBB-vJZB50", // ENGLISH SPEECH | MALALA YOUSAFZAI - Nobel Peace Prize (English Subtitles) - 4
+    "jG-4kBIDAzA", // ENGLISH SPEECH | SELENA GOMEZ: Trust Yourself (English Subtitles) - 5
+    "r01DOmPpxjg", // ENGLISH SPEECH | MUNIBA MAZARI: Motivational Words (English Subtitles) - 6
+    "nIwU-9ZTTJc", // ENGLISH SPEECH | EMMA WATSON: Gender Equality (English Subtitles) - 7
+    "aDG1T0kJnd4", // ENGLISH SPEECH | PRIYANKA CHOPRA: Be Fearless (English Subtitles) - 8
+    "_nUDuXRh3zM", // ENGLISH SPEECH | IVANKA TRUMP: Think Big Again (English Subtitles) - 9
+    "_POoaOQ2Xuc", // ENGLISH SPEECH | PRIYANKA CHOPRA: Full Power of Women (English Subtitles) - 10
+    "EQYWOQ-12c8", // ENGLISH SPEECH | RASHMIKA MANDANNA: Dream BIG! (English Subtitles) - 11
+    "U8iy5zvXhJc", // ENGLISH SPEECH | SHAKIRA: Education Changes the World (English Subtitles) - 12
+    "2mqQMA7CSLQ", // ENGLISH SPEECH | DONALD TRUMP: Never, Ever Give Up (English Subtitles) - 13
+    "mK53d6aIG48", // ENGLISH SPEECH | R. MADHAVAN: India in 2030 (English Subtitles) - 14
+    "KLe7Rxkrj94", // ENGLISH SPEECH | THE ROCK: Be Yourself (English Subtitles) - 15
   ];
 
   const spanishVideoList = [
     // https://www.youtube.com/@EasySpanish
-    // Easy Spanish Podcast - 01
-    // Spanish from Argentina - 02
-    // International episodes - 03
-    // Spanish from Spain - 04
-    // Funny Episodes - 05
-    // Super Easy Spanish - Grammar and Vocab Explained - 06
-    // Spanish from Mexico - 07
-    // Easy Spanish - Learning Spanish from the Streets - 08
+    // Easy Spanish - Learning Spanish from the Streets
     "L_-KQ90IiF0", // Professions | Easy Spanish 1
     "WlknUGwKHtw", // Plans for today | Easy Spanish 2
     "W0_GwKe1SB8", // What do you like about Lima? | Easy Spanish 3
@@ -69,7 +62,6 @@ function loadNextVideo(languages) {
     "M3en0xK1XCk", // Mexican Bread | Easy Spanish 13
     "7GyCg4N6Q-4", // Machu Picchu | Easy Spanish 14
     "cx8LpiW174g", // What did you dream last night? | Easy Spanish 15
-    // Super Easy Spanish - Spanish for Beginners - 09
   ];
 
   if (languages.english) videoList = videoList.concat(englishVideoList);
